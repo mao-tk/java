b017a7cd45cf8379038129d3352790f26a6b2892
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-07-19 14:42:46 +0000

    [2-1]

--- Main.java
+++ Main.java
@@ -1,12 +1,10 @@
 public class Main {
   public static void main (String[] args){
-    final double PI = 3.14;
-    int pie = 5;
-    System.out.println("半径" + pie + "cmのパイの面積は、");
-    System.out.println(pie * pie * PI);
-    System.out.println("パイの半径を倍にします");
-    pie = 10;
-    System.out.println("半径" + pie + "cmのパイの面積は、");
-    System.out.println(pie * pie * PI);
+    int a;
+    int b;
+    a = 20;
+    b = a + 5;
+    System.out.println(a);
+    System.out.println(b);
   }
 }
