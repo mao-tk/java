98dcc2bb4d4e1fb6229632e821c84bde49c98e65
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-07-21 04:13:12 +0000

    [3-4]冗長でスッキリしないソースコード

--- Main.java
+++ Main.java
@@ -1,10 +1,15 @@
 public class Main {
   public static void main (String[] args){
-    boolean tenki = true;
-    if (tenki == true) {
-      System.out.println("洗濯をします");
-      System.out.println("散歩にいきます");
-    } else
-      System.out.println("DVDをみます");
+    System.out.println("あなたの運勢を占います");
+    int fortune = new java.util.Random().nextInt(4) + 1;
+    if (fortune == 1) {
+      System.out.println("大吉");
+    } else if (fortune == 2) {
+      System.out.println("中吉");
+    } else if (fortune == 3) {
+      System.out.println("吉");
+    } else {
+      System.out.println("凶");
+    }
   }
 }
