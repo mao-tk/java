9dde40dded26800656971718a2b4a54b9dc3ec12
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-07-21 03:22:51 +0000

    [3-3]波括弧の省略

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 public class Main {
   public static void main (String[] args){
-    boolean doorClose = true;
-    while (doorClose == false) {
-      System.out.println("ノックする");
-      System.out.println("１分待つ");
-    }
+    boolean tenki = true;
+    if (tenki == true) {
+      System.out.println("洗濯をします");
+      System.out.println("散歩にいきます");
+    } else
+      System.out.println("DVDをみます");
   }
 }
