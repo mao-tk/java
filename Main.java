cdc62bb790f02abae720c91ceaaddeb219c143f7
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-07-22 02:10:33 +0000

    [3章練習問題]3-4

--- Main.java
+++ Main.java
@@ -1,13 +1,12 @@
 public class Main {
   public static void main (String[] args){
-    int isHungry = 0;
-    String food = "ネギトロ";
-    System.out.println("こんにちわ");
-    if (isHungry == 0) {
-      System.out.println("お腹がいっぱいです");
+    boolean tenki = true;
+    if (tenki == true) {
+      System.out.println("洗濯をします");
+      System.out.println("散歩にいきます");
     } else {
-      System.out.println(food + "をいただきます");
+      System.out.println("DVDを見ます");
+      System.out.println("寝ます");
     }
-    System.out.println("ごちそうさまでした");
   }
 }
