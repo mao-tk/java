cfa2ab064336d9f447eef621d321759eb06f357f
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-07-23 04:10:35 +0000

    [4-12]DNAの記号をランダムに表示する

--- Main.java
+++ Main.java
@@ -1,12 +1,28 @@
 public class Main {
   public static void main (String[] args){
-    int[] scores = {20, 30, 40, 50, 80};
-    int count = 0;
-    for (int i = 0; i < scores.length; i++) {
-      if (scores[i] >= 50) {
-        count++;
+    int[] seq = new int[10];
+    
+    // 塩基配列をランダムに生成
+    for (int i = 0; i < seq.length; i++) {
+      seq[i] = new java.util.Random().nextInt(4);
+    }
+    
+    // 生成した塩基配列の記号を表示
+    for (int i = 0; i < seq.length; i++) {
+      switch (seq[i]) {
+        case 0:
+          System.out.print("A ");
+          break;
+        case 1:
+          System.out.print("T ");
+          break;
+        case 2:
+          System.out.print("G ");
+          break;
+        case 3:
+          System.out.print("C ");
+          break;
       }
     }
-    System.out.println("50点以上の科目数:" + count);
   }
 }
