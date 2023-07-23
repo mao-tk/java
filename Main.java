08821fd38ec3627066896200c2f6a5a6a910e4e8
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-07-23 04:19:19 +0000

    [4-14]拡張for文

--- Main.java
+++ Main.java
@@ -1,16 +1,8 @@
 public class Main {
   public static void main (String[] args){
-    int[] seq = new int[10];
-    
-    // 塩基配列をランダムに生成
-    for (int i = 0; i < seq.length; i++) {
-      seq[i] = new java.util.Random().nextInt(4);
-    }
-    
-    // 生成した塩基配列の記号を表示
-    for (int i = 0; i < seq.length; i++) {
-      char[] base = {'A', 'T', 'G', 'C'};
-      System.out.print(base[seq[i]] + " ");
+    int[] scores = {20, 30, 40, 50, 80};
+    for (int value : scores) {
+      System.out.println(value);
     }
   }
 }
