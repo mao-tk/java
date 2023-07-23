000b083f9736fbf87c54c6bf720bea3492a4664f
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-07-23 02:29:04 +0000

    [4-4]配列の長さを調べる

--- Main.java
+++ Main.java
@@ -1,15 +1,7 @@
 public class Main {
   public static void main (String[] args){
-    int sansu = 20;
-    int kokugo = 30;
-    int rika = 40;
-    int eigo = 50;
-    int syakai = 80;
-    
-    int sum = sansu + kokugo + rika + eigo + syakai;
-    int avg = sum / 5;
-    
-    System.out.println("合計点:" + sum);
-    System.out.println("平均点:" + avg);
+    int[] scores = new int[5];
+    int num = scores.length;
+    System.out.println("要素の数:" + num);
   }
 }
