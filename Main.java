e7435eb7f56b58baf71d38db820ce4d78b1a1803
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-07-29 06:33:30 +0000

    [4章練習問題]4-2

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
 public class Main {
   public static void main (String[] args){
-    int[] points = new int[4];
-    double[] wights = new double[5];
-    boolean[] answers = new boolean[3];
-    String[] names = new String[3];
+    int[] moneyList ={121902, 8302, 55100};
+    for (int i = 0; i < moneyList.length; i++) {
+      System.out.println(moneyList[i]);
+    }
+    for (int m : moneyList) {
+      System.out.println(m);
+    }
   }
 }
