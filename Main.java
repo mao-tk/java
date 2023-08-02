cfb5d14120a85cc5a83d3b042e53d1e2c8ae2f97
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-08-02 14:29:53 +0000

    [5-4]引数の例(渡す値が1つの場合)

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
 public class Main {
-  public static void methodA() {
-    System.out.println("methodA");
-    methodB();
-  }
-  public static void methodB() {
-    System.out.println("methodB");
-  }
   public static void main(String[] args) {
-    methodA();
+    System.out.println("メソッドを呼び出します");
+    hello("minato");
+    hello("asaka");
+    hello("sugawara");
+    System.out.println("メソッドの呼び出しが終わりました");
+  }
+  public static void hello(String name) {
+    System.out.println(name + "さん、こんにちわ");
   }
 }
