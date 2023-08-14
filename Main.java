7c8a256db07f1a62b75b9b4606ec232a2af31d6e
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-08-14 13:04:47 +0000

    [5-10]オーバーロード（引数の型が異なる場合）

--- Main.java
+++ Main.java
@@ -1,9 +1,19 @@
 public class Main {
+  // 1つ目のaddメソッド
   public static int add(int x, int y) {
-    int ans = x + y;
-    return ans;
+    return x + y;
+  }
+  // 2つ目のaddメソッド
+  public static double add(double x, double y) {
+    return x + y;
+  }
+  // 3つ目のaddメソッド
+  public static String add(String x, String y) {
+    return x + y;
   }
   public static void main(String[] args) {
-    System.out.println(add(add(10, 20), add(30, 40)));
+    System.out.println(add(10, 20));
+    System.out.println(add(3.5, 2.7));
+    System.out.println(add("Hello", "World"));
   }
 }
