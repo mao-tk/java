4b9fc24502d0093646486ab42a7e083fe27ef639
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-08-20 13:35:23 +0000

    [6-1]計算機プログラム

--- Main.java
+++ Main.java
@@ -1,15 +1,14 @@
 public class Main {
-  public static int[] makeArray(int size) {
-    int[] newArray = new int[size];
-    for (int i = 0; i < newArray.length; i++ ) {
-      newArray[i] = i;
-    }
-    return newArray;
-  }
   public static void main(String[] args) {
-    int[] array = makeArray(3);
-    for (int i : array) {
-      System.out.println(i);
-    }
+    int a = 10; int b = 2;
+    int total = tasu(a, b);
+    int delta = hiku(a, b);
+    System.out.println("足すと" + total + "、引くと" + delta);
+  }
+  public static int tasu(int a, int b) {
+    return (a + b);
+  }
+  public static int hiku(int a, int b) {
+    return (a - b);
   }
 }
