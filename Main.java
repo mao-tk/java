51d6746a2cba8d6086753fd84ff5b8b9afe342f2
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-08-22 13:28:15 +0000

    [6-2]Calc.javaに計算処理を追加する(エラー)

--- Main.java
+++ Main.java
@@ -1,10 +1,4 @@
-public class Main {
-  public static void main(String[] args) {
-    int a = 10; int b = 2;
-    int total = tasu(a, b);
-    int delta = hiku(a, b);
-    System.out.println("足すと" + total + "、引くと" + delta);
-  }
+public class Main{
   public static int tasu(int a, int b) {
     return (a + b);
   }
