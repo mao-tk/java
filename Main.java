46072239736468d9ea5f5e0d6fc37f500cda7a39
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-09-03 13:40:52 +0000

    [6-10]APIの利用

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
-public class Main{
-  public static int tasu(int a, int b) {
-    return (a + b);
-  }
-  public static int hiku(int a, int b) {
-    return (a - b);
+public class Main {
+  public static void main(String[] args) {
+    int[] heights = {172, 149, 152, 191, 155};
+    java.util.Arrays.sort(heights);
+    for (int h : heights) {
+      System.out.println(h);
+    }
   }
 }
