ab6cd1d63d13b9683e1487dc5adb1adb67be27c8
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-09-06 14:23:11 +0000

    問題

--- Main.java
+++ Main.java
@@ -1,10 +1,16 @@
-import comment.Zenhan;
-
 public class Main {
-  public static void main(String[] args) throws Exception {
-    Zenhan.doWarusa();
-    Zenhan.doTogame();
-    comment.Kouhan.callDeae();
-    comment.Kouhan.showMondokoro();
+  public static void main(String[] args) {
+    for (int a = 0; a < 10; a++) {
+      System.out.print("BLOCK" + String.format("%02d ", a));
+      for (int b = 0; b < 10; b++) {
+        if (a == b) {
+          System.out.print("00 ");
+        }else {
+          System.out.print(String.format("%02d ", a * 10 + b));
+        }
+      }
+      System.out.println("");
+    }
+    System.out.println("END");
   }
 }
